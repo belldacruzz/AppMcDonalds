ba58f606f4bd9ae442d96829d4b17f19b1cb4ddb
ISABELLE SANTOS DA CRUZ <isabelle.6973@aluno.pr.senac.br>
2026-05-13 16:53:29 -0300

    Renomeando imgs do projeto, aplicando const AppImages para padronizar e fácil usabilidade

---
+++ data/images.ts
@@ -0,0 +1,24 @@
+import type {ImageSourcePropType} from 'react-native';
+
+export const Appimages = {
+restaurantCover: require('@assets/images/estabelecimento.png') as ImageSourcePropType,
+logo: require('@assets/images/logo.png') as ImageSourcePropType,
+productComboDuploBicMac : require('@assets/images/comboduplobicmac.png') as ImageSourcePropType,
+productComboBraboMeltOnionRings : require('@assets/images/combobrabomeltonionrings.png') as ImageSourcePropType,
+productComboCheddarDuplo : require('@assets/images/combocheddarduplo.png') as ImageSourcePropType,
+productComboMcCrispyChickenElite : require('@assets/images/combomccrispychickenelite.png') as ImageSourcePropType,
+productLancheBicMac : require('@assets/images/lanchebicmac.png') as ImageSourcePropType,
+productLancheCheddarMcMelt : require('@assets/images/lanchecheddarduplo.png') as ImageSourcePropType,
+productLancheDuploCheddarMcMelt: require('@assets/images/lancheduplocheddar.png') as ImageSourcePropType,
+productLancheDuploQuarteirao : require('@assets/images/lanchemccrispychickenelite.png') as ImageSourcePropType,
+productLancheMcnifico : require('@assets/images/lanchemcnifico.png') as ImageSourcePropType,
+productSorveteBaunilha : require('@assets/images/sorvetebaunilha.png') as ImageSourcePropType,
+productSorveteChocolate : require('@assets/images/sorvetechocolate.png') as ImageSourcePropType,
+productSorveteMisto : require('@assets/images/sorvetemisto.png') as ImageSourcePropType,
+productBatataFritaGrande : require('@assets/images/batatafritagrande.png') as ImageSourcePropType,
+productBatataFritaMedia : require('@assets/images/batatafritamedia.png') as ImageSourcePropType,
+productBatataFritaPequena : require('@assets/images/batatafritapequena.png') as ImageSourcePropType,
+productBebidaAguaMineral : require('@assets/images/aguamineral.png') as ImageSourcePropType,
+ProductBebidaCocaCola : require('@assets/images/cocacola.png') as ImageSourcePropType,
+ProductBebidaFantaLaranja : require('@assets/images/fantalaranja.png') as ImageSourcePropType,
+} 
